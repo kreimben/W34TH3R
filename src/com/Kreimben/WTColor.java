03e52078bec298c242dfaa8912eb6e4f36c83a9b
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-12-16 03:17:38 +0900

    Implement color presets

---
+++ src/com/Kreimben/WTColor.java
@@ -0,0 +1,24 @@
+package com.Kreimben;
+
+import java.awt.*;
+
+public enum WTColor {
+
+    BLUE(0, 122, 255),
+    GREEN(52, 199, 89),
+    INDIGO(88, 86, 214),
+    ORANGE(255, 149, 0),
+    PINK(255, 45, 85),
+    PURPLE(175, 82, 222),
+    RED(255, 59, 48),
+    TEAL(90, 200, 250),
+    YELLOW(255, 204, 0);
+
+    int r, g, b;
+
+    private WTColor(int r, int g, int b) { }
+
+    public Color getColor() {
+        return new Color(r, g, b);
+    }
+}
