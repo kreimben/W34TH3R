c4eb5e34747cc80fd1ed6829c3415fcb310c0335
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-12-19 03:19:10 +0900

    Make folder and Rearrange files.

--- src/com/Kreimben/WTCompletion.java
+++
@@ -1,8 +0,0 @@
-package com.Kreimben;
-
-import java.util.HashMap;
-
-@FunctionalInterface
-public interface WTCompletion {
-    void completion();
-}
--- src/com/Kreimben/WTMapCompletion.java
+++
@@ -1,8 +0,0 @@
-package com.Kreimben;
-
-import java.util.HashMap;
-
-@FunctionalInterface
-public interface WTMapCompletion {
-    HashMap<String, String> completion();
-}
