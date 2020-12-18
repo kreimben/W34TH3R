a654e267bf2d73f3a01e9ab1926eeec145148854
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-12-18 22:11:34 +0900

    Implement list with scroll pane.

--- src/com/Kreimben/WTSelectCityViewController.java
+++ src/com/Kreimben/WTSelectCityViewController.java
@@ -2,9 +2,13 @@ package com.Kreimben;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Arrays;
+import java.util.Date;
 
 public class WTSelectCityViewController extends JFrame {
 
@@ -14,20 +18,24 @@ public class WTSelectCityViewController extends JFrame {
     private int windowWidth = 400;
 
     public WTSelectCityViewController() {
+
         var size = new Dimension(this.windowWidth, 500);
+
         setSize(size);
         setResizable(false);
         setLocationRelativeTo(null);
 
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-        this.setBasicComponent(null);
+        this.setBasicComponent(() -> {
+
+        });
     }
 
     private void setBasicComponent(WTCompletion completion) {
 
         this.makeTextField();
-        this.makeList();
+        this.makeScrollList(this.testFunction(200));
 
         this.setVisible(true);
 
@@ -39,11 +47,13 @@ public class WTSelectCityViewController extends JFrame {
         this.textField = new JTextField();
 
         this.textField.setBorder(new EmptyBorder(24, 12, 12, 0));
-        this.textField.setSize(this.windowWidth, 24);
 
-        System.out.format("text field size: %s\n", this.textField.getSize());
         this.textField.setOpaque(true);
-        //this.textField.setBackground(new Color(255, 0, 0));
+        this.textField.setBackground(WTColor.TEAL.getColor());
+
+        this.textField.setSize(this.windowWidth, 30);
+
+        System.out.format("Text field's size is %s\n", this.textField.getSize());
 
         this.textField.addKeyListener(new KeyListener() {
             @Override
@@ -65,17 +75,38 @@ public class WTSelectCityViewController extends JFrame {
         this.add(this.textField);
     }
 
-    private void makeList() {
+    private String[] testFunction(int numberOfRepeat) {
+        var result = new String[0];
+
+        for (int i = 0; i < numberOfRepeat; i++) {
+
+            result = Arrays.copyOf(result, result.length + 1);
+            result[result.length - 1] = String.valueOf(i);
+        }
+
+        return result;
+    }
+
+    private void makeScrollList(String[] data) {
 
         this.listOfCities = new JList();
 
-        System.out.format("List info: %s\n", this.listOfCities);
-
         this.listOfCities.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         this.listOfCities.setLayoutOrientation(JList.VERTICAL);
-        this.listOfCities.setVisibleRowCount(-1);
         this.listOfCities.setBorder(new EmptyBorder(24, 8, 8, 8));
+        this.listOfCities.setVisibleRowCount(25);
+        this.listOfCities.setOpaque(true);
+        this.listOfCities.setBackground(WTColor.BLUE.getColor());
 
-        this.add(this.listOfCities);
+        this.listOfCities.setListData(data);
+
+        this.listOfCities.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                System.out.format("This is selected as city! %s\n", e.getLastIndex());
+            }
+        });
+
+        this.add(new JScrollPane(this.listOfCities));
     }
 }
